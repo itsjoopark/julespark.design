2bf1fc9b9231a17da4a02d8172dbcf01526b5eaf
Jules <hjoo.edu@gmail.com>
2025-12-03 00:55:16 -0500

    Remove nav bar from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "@/components/Header";
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#f0efea]">
-      <Header />
-      
-      {/* Main content area - clean empty space matching Figma */}
-      <main className="pt-[89px] min-h-screen">
-        {/* Empty for now - matches Figma landing page design */}
+      {/* Main content area - clean empty space */}
+      <main className="min-h-screen">
+        {/* Content will go here */}
       </main>
     </div>
   );
